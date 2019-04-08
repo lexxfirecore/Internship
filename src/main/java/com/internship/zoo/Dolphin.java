efa79e7e7de35964cf395a7ac4cf8aa53198ed55
“Margarita <margaritaq@mail.ru>
2019-04-08 11:46:13 +0300

    Margarita sarcina 4 REmodificata dupa standartu lu Sandu

--- src/main/java/com/internship/zoo/Dolphin.java
+++ src/main/java/com/internship/zoo/Dolphin.java
@@ -4,42 +4,38 @@ package com.internship.zoo;
  * Created by Margarita on 04-Apr-19.
  */
 public class Dolphin {
-    private String name= "Swit";
-    private int age= 5;
-    private int height = 5;
-    private float weight=100;
-    private int intellegence=50;
+    private String name;
+    private int age;
+    private int height;
+    private float weight;
+    private int intellegence;
 
     public Dolphin() {
-        System.out.println("Delfinul a fost creat!");
+
     }
 
     public Dolphin(String name, int age) {
         this.name = name;
         this.age = age;
-        System.out.println("Name: "+name+", Age: "+age);
     }
 
     public Dolphin(String name, int age, int height) {
-        this.name = name;
-        this.age = age;
+        this(name, age);
         this.height = height;
-        System.out.println("Name: "+name+", Age: "+age+", Height: "+height);
     }
 
-    public Dolphin(String name, int age, int height, float weight) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
+
+    public void say() {
+        System.out.println("Dolphin says: ioooii  ");
     }
 
-    public Dolphin(String name, int age, int height, float weight, int intellegence) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.intellegence = intellegence;
+    public void swim() {
+        System.out.println("Delfinii pot sa inoate cu o viteza de 55,5 Km/h");
+
+    }
+
+    public void jump() {
+        System.out.println("Delfinii des sar pentru distractie");
     }
 
     public String getName() {
@@ -80,17 +76,5 @@ public class Dolphin {
 
     public void setIntellegence(int intellegence) {
         this.intellegence = intellegence;
-    }
-
-
-    public static void say(){
-        System.out.println("Dolphin says: ioooii  ");
-    }
-    public static void swim(){
-        System.out.println(" Delfinii pot sa inoate cu o viteza de 55,5 Km/h");
-
-    }
-    public static void jump(){
-        System.out.println("Delfinii des sar pentru distractie");
     }
 }
