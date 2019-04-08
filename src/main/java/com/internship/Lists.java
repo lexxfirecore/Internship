2195df40200a2ac31aafa7fe2470a601e8313ab7
VMarian <vasea2984@gmail.com>
2019-04-08 22:17:57 +0300

    sarcina 5

--- src/main/java/com/internship/Lists.java
+++ src/main/java/com/internship/Lists.java
@@ -1,6 +1,9 @@
 package com.internship;
 
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * Created by lexx on 03-Apr-19.
@@ -18,8 +21,28 @@ public class Lists {
     }
 
     private static void lists_2() {
+/**
+ * Created by vmarian on 08-Apr-19.
+ */
+            String[] date = {"11", "12", "13","14", "15"};
+            ArrayList<String> number = new ArrayList<String>();
+            for (int i = 0; i < date.length; i++) {
+                number.add(date[i]);
 
+                System.out.println(number);
+                String str = new String("2");
+                ((ArrayList<String>)number).add(str);
+                System.out.println(number);
+                Map<Integer, String> map = new TreeMap<>();
+                map.put(1, (String) "17");
+                map.put(3, (String) "19");
+                System.out.println(map);
+                System.out.println(map.get(22));
+                System.out.println(((TreeMap<Integer, String>) map).clone());
+            }
     }
+
+
 
     private static void lists_3() {
 
