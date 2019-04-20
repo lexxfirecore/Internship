e8797ddaeb63f0829b960db9ec3edf8e9b194781
Ion Ghincolov <keregann@gmail.com>
2019-04-20 01:50:32 +0100

    Ion - Task 3  - apache.common.lang library added through idea and maven  - required methods implemented

--- src/main/java/com/internship/ApacheStringUtils.java
+++ src/main/java/com/internship/ApacheStringUtils.java
@@ -3,6 +3,9 @@ package com.internship;
 /**
  * Created by lexx on 02-Apr-19.
  */
+
+import org.apache.commons.lang3.StringUtils;
+
 public class ApacheStringUtils {
     public static void main(String[] args) {
         System.out.println(ApacheStringUtils.class.getName() + ": ");
@@ -10,6 +13,11 @@ public class ApacheStringUtils {
         isEmptyExample();
         startsWithExample();
 
+        //added by Ion - 20/04/19
+        indexOfExample();
+        rotateExample();
+        substringAfterExample();
+        isBlankExample();
         // TODO: si asa mai departe pentru fiecare metoda
 
     }
@@ -22,5 +30,37 @@ public class ApacheStringUtils {
         // TODO: insert your code here
     }
 
+    // Created by Ion
+    private static void indexOfExample(){
+        System.out.println("indexOf() method example by Ion");
+            String s = "IndexOf";
+        System.out.println("Index of 'n' in " + s + " : " + StringUtils.indexOf(s, 'n'));
+    }
+    // Created by Ion
+    private static void rotateExample(){
+        System.out.println("rotate() method example by Ion");
+        String s = "Rotate";
+        System.out.println("Original string : " + s);
+        System.out.println( s + " : " + StringUtils.rotate(s, 3));
+        System.out.println("Original string : " + s);
+        System.out.println( s + " : " + StringUtils.rotate(s, 5));
+    }
+    // Created by Ion
+    private static void substringAfterExample(){
+        System.out.println("substringAfter() method example by Ion");
+        String s = "substringAfter";
+        System.out.println("Original string : " + s);
+        System.out.println( s + " : " + StringUtils.substring(s, 3));
+        System.out.println("Original string : " + s);
+        System.out.println( s + " : " + StringUtils.substring(s, 3, 9));
+    }
+    // Created by Ion
+    private static void isBlankExample(){
+        System.out.println("isBlankExample() method example by Ion");
+        String s = "isBlank?";
+        System.out.println("Original string : " + s);
+        System.out.println( s + " : " + StringUtils.isBlank(s));
+
+    }
 
 }
